a2755303b14450c7b367dc0ba68aac3cf591deb3
Dario Russo <dario.russo@bbc.co.uk>
2022-04-28 10:07:21 +0100

    boilerplate start

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const simorghSrcPath =''
+const psammeadPackages = ''
+
+// Remove Files
+
+//Create legacy Folder
+
+// Move files to Simorgh
+
+// Change import location
